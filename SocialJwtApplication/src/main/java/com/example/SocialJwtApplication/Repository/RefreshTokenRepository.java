ab293f6249ff75990e3367158db0149cc24ed336
VikasKumar8354 <120902vikaskumar@gmail.com>
2025-11-06 19:49:23 +0530

    @Repository Annotation Add

--- SocialJwtApplication/src/main/java/com/example/SocialJwtApplication/Repository/RefreshTokenRepository.java
+++ SocialJwtApplication/src/main/java/com/example/SocialJwtApplication/Repository/RefreshTokenRepository.java
@@ -3,9 +3,10 @@ package com.example.SocialJwtApplication.Repository;
 import com.example.SocialJwtApplication.Model.RefreshToken;
 import com.example.SocialJwtApplication.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken,Long> {
 
     Optional<RefreshToken> findByToken(String token);
